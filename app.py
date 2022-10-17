885d789f175a35004ee569705395633927af62c5
Sumanth <109844520+Simply-Sumanth@users.noreply.github.com>
2022-10-17 22:37:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+from classes import MainWin
+from tkinter import *
+
+if __name__ == "__main__":
+	mainWindow = MainWin("Student Entry/Exit through QR","800x500")
+	mainWindow["background"] = "#9fafca"
+	mainWindow.open()
